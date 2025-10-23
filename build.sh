bcd78624194f4b6d34297fef2f17bb3bafcb6515
Hugh JF Chen <hugh.jf.chen@gmail.com>
2025-10-23 09:47:14 +0800

    add install scrip to install a service to update my site.

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# update the content first
+git pull
+
+# build it
 emacs -Q --script build-site.el
+
+# now copy the built content to the web server
+ORIG_OWNER=$(stat -c '%U:%G' /var/www/index.html)
 sudo cp -R ./public/* /var/www/
+sudo chown -R "$ORIG_OWNER" /var/www/*
---
+++ serve.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# this script will install a systemd service which
+# will update the content from git repo, build it
+# and then publish to the web server
+
+SERVICE_NAME="update-my-site.service"
+TIMER_NAME="update-my-site.timer"
+WD=$($PWD/$(dirname $0))
+
+UPDATE_MY_SITE_SERVICE="
+[Unit]
+
+[Service]
+WorkingDirectory=$WD
+User=$USER
+ExecStart=$WD/build.sh
+Type=oneshot
+
+"
+
+echo "$UPDATE_MY_SITE_SERVICE" | sudo tee /etc/systemd/system/$SERVICE_NAME
+
+UPDATE_MY_SITE_TIMER="
+[Unit]
+
+[Timer]
+OnBootSec=6m
+OnUnitActiveSec=6m
+Unit=$SERVICE_NAME
+
+
+[Install]
+WantedBy=timers.target
+
+"
+
+echo "$UPDATE_MY_SITE_TIMER" | sudo tee /etc/systemd/system/$TIMER_NAME
+
+sudo systemctl daemon-reload
+sudo systemctl enable $SERVICE_NAME
+sudo systemctl enable $TIMER_NAME
